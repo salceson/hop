b74f7f76a2305d693c632b0092180a6e163ed76b
salceson <ciolczyk.michal.93@gmail.com>
2015-01-24 12:36:08 +0100

    Some steps

---
+++ configuration.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+__author__ = 'Michał Ciołczyk'
+
+
+class Configuration(object):
+    # Velocities
+    @staticmethod
+    def walking_velocity():
+        return 0.1
+
+    @staticmethod
+    def taxi_velocity():
+        return 2
+
+    # Distances
+    @staticmethod
+    def distances_map():
+        return {
+            'park': {
+                'home': 80,
+            },
+            'home': {
+                'park': 80,
+            },
+        }
+
+    @staticmethod
+    def distance(from_place, to_place):
+        return Configuration.distances_map()[from_place][to_place]
+
+    # Buses
+    @staticmethod
+    def can_get_to_bus(from_place):
+        bus_locations = ['park', 'intersection', 'school']
+        return bus_locations.__contains__(from_place)
+
+    @staticmethod
+    def bus_frequency_per_hour():
+        return 6
--- operators.py
+++ operators.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+from configuration import Configuration
+
 __author__ = 'Michał Ciołczyk'
 
 
@@ -9,6 +11,7 @@ def taxi_fare(dist):
 def walk(state, who, from_place, to_place):
     if state.location[who] == from_place:
         state.location = to_place
+        state.time += Configuration.distance(from_place, to_place) / Configuration.walking_velocity() * 60
         return state
     else:
         return False
@@ -16,5 +19,28 @@ def walk(state, who, from_place, to_place):
 
 def call_taxi(state, _, from_place):
     state.location['taxi'] = from_place
+    state.time += 10
     return state
 
+
+def drive_taxi(state, _, from_place, to_place):
+    if from_place != state.location['taxi']:
+        return False
+    state.location['taxi'] = to_place
+    state.location['me'] = to_place
+    state.time += Configuration.distance(from_place, to_place) / Configuration.taxi_velocity() * 60
+    return state
+
+
+def get_into_bus(state, _, from_place):
+    if Configuration.can_get_to_bus(from_place):
+        return False
+    state.location['bus'] = from_place
+    bus_dt = int(60.0 / Configuration.bus_frequency_per_hour())
+    if state.time % bus_dt > 0:
+        state.time += bus_dt - state.time % bus_dt
+    return state
+
+
+def drive_bus(state, _, from_place):
+    pass
