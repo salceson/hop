f9e21164891c78365fcd6eaebdb75647578a4402
salceson <ciolczyk.michal.93@gmail.com>
2015-01-27 00:48:04 +0100

    Draw city, map, readme update

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ MSI - Pyhop travelling example
 
 Pyhop travelling example with more features.
 
+## City
+
+![City](map.png)
+
 ## Operators
 
 Some description will follow here.
--- utils/draw_city.py
+++ utils/draw_city.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python
 # -*- coding=utf-8 -*-
+
 __author__ = 'Michał Ciołczyk'
 
 from configuration import Configuration
+from matplotlib.pyplot import plot, savefig
+import networkx as nx
 
 output_filename = '../map.png'
 
 dist_map = Configuration.distances_map()
 vertices = dist_map.keys()
+
+graph = nx.Graph()
+
+for x in vertices:
+    graph.add_node(x)
+
+for x in vertices:
+    for y in vertices:
+        if dist_map[x].get(y) is not None:
+            graph.add_edge(x, y)
+
+edges_labels = dict(((u, v), dist_map[u][v]) for (u, v) in graph.edges())
+nodes_labels = dict((x, x) for x in vertices)
+
+pos = nx.spectral_layout(graph)
+nx.draw(graph, pos, node_size=3500, font_size=16, font_color="white", font_weight="bold", labels=vertices)
+nx.draw_networkx_edge_labels(graph, pos=pos, edge_labels=edges_labels, alpha=0.1)
+nx.draw_networkx_labels(graph, pos, nodes_labels, font_size=10)
+savefig(output_filename)
